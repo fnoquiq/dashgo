8c67632e256808a68172513d1f6d194e58fe7964
Gabriel Mesquita <gabrielteixeiramesquita@gmail.com>
2021-07-19 18:04:43 -0300

    :rocket: Fluxo de navegação

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -1,5 +1,6 @@
 import { Box, Flex, Heading, Button, Icon, Table, Thead, Th, Tr, Td, Checkbox, Tbody, Text, useBreakpointValue } from "@chakra-ui/react";
-import { RiAddLine, RiPencilLine } from "react-icons/ri";
+import Link from "next/link";
+import { RiAddLine } from "react-icons/ri";
 
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
@@ -22,15 +23,18 @@ export default function UserList() {
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">Usuários</Heading>
 
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
-            >
-              Criar novo
-            </Button>
+            <Link href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20"/>}
+              >
+                Criar novo
+              </Button>
+            </Link>
+            
           </Flex>
 
           <Table colorScheme="whiteAlpha">
